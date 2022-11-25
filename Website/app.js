26f64ce74d794d0dba183bdde7e12dc5833ca8a7
OliviaCorrigan <109996353+OliviaCorrigan@users.noreply.github.com>
2022-11-25 17:09:12 +0000

    Add files via upload

---
+++ Website/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+
+    const className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "Dark";
+    } else {
+        this.textContent = "Light";
+    }
+});
+`use strict`;
+function refreshTime() {
+  const timeDisplay = document.getElementById("time");
+  const dateString = new Date().toLocaleString();
+  const formattedString = dateString.replace(", ", " - ");
+  timeDisplay.textContent = formattedString;
+}
+  setInterval(refreshTime, 1000);
+
+  const button = document.querySelector('input');
+const fieldset = document.querySelector('fieldset');
+
+button.addEventListener('click', disableButton);
+
+function disableButton() {
+  fieldset.disabled = true;
+  setTimeout(() => {
+    fieldset.disabled = false;
+  }, 2000);
+}
+
+
